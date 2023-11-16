7c58a322f60d34570f6c9b523e73f0ad154a3dea
Israa <90574971+israad1@users.noreply.github.com>
2023-11-16 12:16:36 -0500

    update technicien class header file

--- include/libclean/technician.hpp
+++ include/libclean/technician.hpp
@@ -1,18 +1,20 @@
-#ifndef LIBCLEAN_TECHNICIAN_HPP
-#define LIBCLEAN_TECHNICIAN_HPP
-#include <deque>
-#include "libclean/robot.hpp" // Include the Robot class header
+#ifndef LIBCLEAN_TECHNICIAN_H
+#define LIBCLEAN_TECHNICIAN_H
+#include <list>
+#include "libclean/Technician.h" // Include the Robot class header
 #include <iostream>
-
+using namespace std;
 class Technician {
-    private:
-        std::deque<Robot*> brokenRobotQueue;
 
-    public:
-        Technician();
-        void technicianFixesRobot();
-        void addRobotToQueue(Robot* robot);
-        bool isTechBusy() const;
+public:
+
+    Technician();
+
+    void addRobotToQueue(Robot& robot);
+    bool isFixed(Robot& robot);
+    bool isTechBusy() const;
+    void Robot_isOkay(Robot& robot);
+    list<Robot> Busy_robot_queue;
 };
 
 #endif // TECHNICIAN_H
