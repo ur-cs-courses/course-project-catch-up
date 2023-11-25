7b59b04d69339e2f2af8de5ece8c631c2d3857fa
SofDimotsi <sofia.dimotsi@richmond.edu>
2023-11-25 15:15:50 -0500

    changes to return values

--- src/room.cpp
+++ src/room.cpp
@@ -11,10 +11,10 @@ Room::Room(std::string name, float width_, float length, bool sweepable, bool mo
 {
     this->filename = filename;
     std::ofstream file(filename);
-    if (!file.is_open()) {
+    //if (!file.is_open()) {
         // Handle file opening error
-        return;
-    }
+      //  return;
+    //}
     file << "Room object created" << std::endl; 
 
     this->name = name;
@@ -42,20 +42,12 @@ Room::Room(std::string name, float width_, float length, bool sweepable, bool mo
 
 std::string Room::getName() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getName() function was called" << std::endl; 
     return name;
 };
 
 float Room::getWidth() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getWidth() function was called" << std::endl; 
 
     return width_;
@@ -63,90 +55,54 @@ float Room::getWidth() {
 
 float Room::getLength() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getLength() function was called" << std::endl; 
     return length_;
 };
 
 float Room::getSize(){
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getSize() function was called" << std::endl; 
     return width_ * length_;
 };
 
 bool Room::getSweepable() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getSweepable() function was called" << std::endl; 
     return sweepable;
 };
 
 bool Room::getMoppable() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getMoppable() function was called" << std::endl; 
     return moppable;
 };
 
 bool Room::getScrubbable() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getScrubbable() function was called" << std::endl; 
     return scrubbable;
 };
 
 float Room::getPercentSwept() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getPercentSwept() function was called" << std::endl; 
     return percentSwept_;
 };
 
 float Room::getPercentMopped() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getPercentMopped() function was called" << std::endl; 
     return percentMopped_;
 };
 
 float Room::getPercentScrubbed() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room getPercentScrubbed() function was called" << std::endl; 
     return percentScrubbed_;
 };
 
 void Room::setPercentSwept(float percent) {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room setPercentSwept() function was called" << std::endl; 
     if(sweepable && percentSwept_ <= 100 && percentSwept_ >= 0){
         percentSwept_ = percent;
@@ -155,10 +111,6 @@ void Room::setPercentSwept(float percent) {
 
 void Room::setPercentMopped(float percent) {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room setPercentMopped() function was called" << std::endl; 
     if(moppable && percentMopped_ <= 100 && percentMopped_ >= 0){
         percentMopped_ = percent;
@@ -168,10 +120,6 @@ void Room::setPercentMopped(float percent) {
 // can't go above 100
 void Room::setPercentScrubbed(float percent) {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room setPercentScrubbed() function was called" << std::endl; 
     if(scrubbable && percentScrubbed_ <= 100 && percentScrubbed_ >= 0){
         percentScrubbed_ = percent;
@@ -180,10 +128,6 @@ void Room::setPercentScrubbed(float percent) {
 
 void Room::randomlyDirty() {
     std::ofstream file(filename);
-    if (!file.is_open()) {
-        // Handle file opening error
-        return;
-    }
     file << "Room randomlyDirty() function was called" << std::endl; 
     if (sweepable && percentSwept_ > 0.01) {
         percentSwept_ -= .01;
